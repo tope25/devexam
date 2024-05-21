be7217c41a23da5001bcd4ae555683f807978191
christopher <bacula.25christopher@gmail.com>
2024-05-22 05:22:15 +0800

    devexam

---
+++ ExamDev/app/src/main/java/exam/dev/examdev/UserData.kt
@@ -0,0 +1,9 @@
+package exam.dev.examdev
+
+data class UserData(
+    val fullName: String,
+    val email: String,
+    val mobileNumber: String,
+    val dateOfBirth: String,
+    val gender: String
+)
