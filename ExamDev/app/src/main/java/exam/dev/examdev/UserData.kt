2e8012e4f7d4eb0b54dfc623c973c93fcf577147
tope25 <161114572+tope25@users.noreply.github.com>
2024-05-22 05:45:54 +0800

    Delete ExamDev directory

--- ExamDev/app/src/main/java/exam/dev/examdev/UserData.kt
+++
@@ -1,9 +0,0 @@
-package exam.dev.examdev
-
-data class UserData(
-    val fullName: String,
-    val email: String,
-    val mobileNumber: String,
-    val dateOfBirth: String,
-    val gender: String
-)
